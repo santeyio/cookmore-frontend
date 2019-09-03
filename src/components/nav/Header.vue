0a0ddcd9fa0a0b99ae63c6869a7b2dbe6f9459c8
Caleb Hayashida <santeyio@gmail.com>
2019-09-03 10:45:24 -0400

    working on nav

--- src/components/nav/Header.vue
+++ src/components/nav/Header.vue
@@ -3,12 +3,21 @@
     <b-navbar sticky="true" variant="light">
       <h1>CookMore</h1>
     </b-navbar>
+    <Slide left>
+      <a id="home" href="#">
+        <span>Home</span>
+      </a>
+    </Slide>
   </div>
 </template>
 
 <script>
+import { Slide } from 'vue-burger-menu';
 export default {
   name: 'Header',
+  components: {
+    Slide,
+  },
 };
 </script>
 
